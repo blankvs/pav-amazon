20ba94a8046c2616394135385e506367348d96aa
Sergio Vega <hekinky@MacBook-Pro.local>
2021-05-26 16:41:58 -0600

    [Layout + Products + Firebase + Database]

---
+++ src/Product.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+
+function Product({title, price, rating, image, id }) {
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Price>${price}</Price>
+      <Rating>
+        {Array(rating)
+          .fill()
+          .map((rating) => (
+            <p>⭐</p>
+          ))}
+      </Rating>
+      <Image src={image} />
+      <ActionSection>
+        <AddToCartButton>Add to Cart</AddToCartButton>
+      </ActionSection>
+    </Container>
+  );
+}
+
+export default Product;
+
+const Container = styled.div`
+  background-color: white;
+  z-index: 1;
+  flex: 1;
+  padding: 20px;
+  margin: 10px;
+  max-height: 400px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Title = styled.span``;
+
+const Price = styled.span`
+  font-weight: 500;
+  margin-top: 3px;
+`;
+
+const Rating = styled.div`
+  display: flex;
+`;
+
+const Image = styled.img`
+  max-height: 200px;
+  object-fit: contain;
+`;
+
+const ActionSection = styled.div`
+  margin-top: 12px;
+  display: grid;
+  place-items: center;
+`;
+
+const AddToCartButton = styled.button`
+  width: 100px;
+  height: 30px;
+  background-color: #f0c14b;
+  border: 2px solid #a88734;
+  border-radius: 2px;
+`;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyC7_Fpfel5Y6X4Lw8UrhTzKNNDa1HtTFhs",
+  authDomain: "clone-project-be286.firebaseapp.com",
+  projectId: "clone-project-be286",
+  storageBucket: "clone-project-be286.appspot.com",
+  messagingSenderId: "420809594090",
+  appId: "1:420809594090:web:4f0ce164ed14d4f8327393",
+  measurementId: "G-N2WM065EF6",
+});
+
+const db = firebase.firestore();
+
+export { db } 
